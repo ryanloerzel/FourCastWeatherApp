62fe3cae638dab10474f2141ac5be40d45b9a85e
ryanloerzel <ryan.loerzel@gmail.com>
2014-06-06 12:03:38 -0700

    Retrieved weather data form the forecast. TO DO NEXT: Parse the JSON data

--- Four Cast/src/main/java/com/spacecasestudios/fourcast/MainActivity.java
+++ Four Cast/src/main/java/com/spacecasestudios/fourcast/MainActivity.java
@@ -14,6 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -83,13 +86,44 @@ public class MainActivity extends ActionBarActivity {
         protected String doInBackground(Object... objects) {
             int responseCode = -1;
 
+            //Attempt a connection with the weather data URL
             try {
                 URL weatherFeedUrl = new URL("https://api.forecast.io/forecast/48ab6b9045bb8c5558c38a43d0e337c0/37.8267,-122.423");
                 HttpURLConnection connection = (HttpURLConnection) weatherFeedUrl.openConnection();
                 connection.connect();
-
                 responseCode = connection.getResponseCode();
-                Log.i(TAG, "Code: " + responseCode);
+
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    InputStream inputStream = connection.getInputStream();
+                    Reader reader = new InputStreamReader(inputStream);
+
+                    //ERROR WHEN REQUESTING CONTENT LENGTH: -1 RETURNED*********************
+//                    int contentLength = connection.getContentLength();
+//                    if(contentLength == -1) {
+//                        //contentLength = connection.getHeaderFieldInt("Length", -1);
+//                        contentLength = 2000;
+//                    }
+//                    Log.v(TAG, Integer.toString(contentLength));
+//                    char[] charArray = new char[contentLength];
+//                    reader.read(charArray);
+//                    String responseData = new String(charArray);
+                    //************************************************************************
+
+                    //THIS fix was written by Hubert Maraszek*********************************
+                    int nextCharacter; // read() returns an int, we cast it to char later
+                    String responseData = "";
+                    while(true){ // Infinite loop, can only be stopped by a "break" statement
+                        nextCharacter = reader.read(); // read() without parameters returns one character
+                        if(nextCharacter == -1) // A return value of -1 means that we reached the end
+                            break;
+                        responseData += (char) nextCharacter; // The += operator appends the character to the end of the string
+                    }
+                    //************************************************************************
+                    Log.v(TAG, responseData);
+                }
+                else {
+                    Log.i(TAG, "Unsuccessful Http Response Code: " + responseCode);
+                }
 
             }
             catch (MalformedURLException e) {
